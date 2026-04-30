da0793c5995b47829f72b6003ae779ddd15f309f
Rakshitha-Kotyan <rakshithakotyan0@gmail.com>
2026-04-30 19:44:21 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Map<Integer, Integer> m = new HashMap<>();
+
+        for(int i=0;i<nums.length;i++){
+            m.put(nums[i],m.getOrDefault(nums[i],0)+1);
+        } 
+
+        for(int i=0;i<nums.length;i++){
+            if(m.get(nums[i])>1){
+                return true;
+            }
+        }
+        return false;
+    }
+}
