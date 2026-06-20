d86e39a6094310396ce1ed09d0e6b51abe1740de
Rakshitha-Kotyan <rakshithakotyan0@gmail.com>
2026-06-20 12:55:40 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int min=Integer.MAX_VALUE;
+        int maxProfit=0;
+        int currentProfit=0;
+        for(int i=0;i<prices.length;i++){
+            if(prices[i]< min){
+                min=prices[i];
+            } else {
+                currentProfit=prices[i]-min;
+                maxProfit=Math.max(maxProfit,currentProfit);
+            }
+        }
+        return maxProfit;
+        
+    }
+}
