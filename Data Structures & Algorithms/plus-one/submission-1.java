9d2365b515277d3f49cf7945095b0cf7219c7101
Rakshitha-Kotyan <rakshithakotyan0@gmail.com>
2026-07-25 15:11:31 +0530

    Add: plus-one - submission-1

---
+++ Data Structures & Algorithms/plus-one/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n=digits.length;
+        
+        int carry=0;
+        for(int i=n-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]+=1;
+                return digits;
+            } else {
+                carry=1;
+                digits[i]=0;
+            }
+        }
+        int ans[]= new int[digits.length+1];
+        if(carry==1){
+            ans[0]=1;
+            for(int i=1;i<ans.length;i++){
+                ans[i]=digits[i-1];
+            }
+        }
+        return ans;
+    }
+}
