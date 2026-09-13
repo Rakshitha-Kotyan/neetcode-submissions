29f7beb04062a7d41495b070f91b713d3bf325e6
Rakshitha-Kotyan <rakshithakotyan0@gmail.com>
2026-09-13 21:33:30 +0530

    Update: is-palindrome - submission-0

--- Data Structures & Algorithms/is-palindrome/submission-0.java
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -1,21 +1,27 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int l=0;
-        int r=s.length()-1;
-
-        while(l<r){
-            while(l<r && !Character.isLetterOrDigit(s.charAt(l))){
-                l++;  
+        int i=0;
+        int j=s.length()-1;
+        while(i<j){
+            while(i<j && !alphaNum(s.charAt(i))){
+                i++;
             }
-            while(l<r && !Character.isLetterOrDigit(s.charAt(r))){
-                r--;  
+            while(j>i && !alphaNum(s.charAt(j))){
+                j--;
             }
-            if(Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))){
+            if(Character.toLowerCase(s.charAt(i))!=Character.toLowerCase(s.charAt(j))){
                 return false;
             }
-            l++;
-            r--;  
+            i++;
+            j--;
+
         }
         return true;
+    }
+
+    public boolean alphaNum(char c) {
+        return (c >= 'A' && c <= 'Z' ||
+                c >= 'a' && c <= 'z' ||
+                c >= '0' && c <= '9');
     }
 }
